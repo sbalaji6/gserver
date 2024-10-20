b6be6ad87db83c458deacfe35cd74a429c3a2b43
Balaji S <balajii.subramanian@gmail.com>
2024-10-20 22:41:16 +0530

    first commit

---
+++ src/main/java/org/example/grpc/GreetingServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.grpc.service;
+
+import com.example.grpc.GreetingServiceGrpc;
+import com.example.grpc.HelloRequest;
+import com.example.grpc.HelloResponse;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        String greeting = "Hello, " + request.getName();
+
+        HelloResponse response = HelloResponse.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
